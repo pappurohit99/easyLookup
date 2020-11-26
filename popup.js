8ec30e47aef755a58af56f94db82ef467c871b87
rpappu <rpappu@mathworks.com>
2020-11-26 15:16:13 +0530

    bugss

--- popup.js
+++ popup.js
@@ -2,13 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
+var cheerio = require('cheerio');
+var request = require('request');
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+var query = 'solve';
+var url = 'https://www.mathworks.com/support/search.html?q=' + query + '&fq=asset_type_name:documentation/function&page=1';
 
-let fetch = document.getElementById('retrieveData');
-fetch.onclick = function () {
-    let text = document.getElementById('fname').value;
-    let url = 'https://www.mathworks.com/support/search.html?q=' + text + '&fq=asset_type_name:documentation/function&page=1';
+var getHTML = function (url, callback) {
+    // Feature detection
+    if (!window.XMLHttpRequest) return;
+
+    // Create new request
+    // eslint-disable-next-line no-undef
+    var xhr = new XMLHttpRequest();
+
+    // Setup callback
+    xhr.onload = function () {
+        if (callback && typeof (callback) === 'function') {
+            callback(this.responseXML);
+        }
+    };
+
+    // Get the HTML
+    xhr.open('GET', url);
+    xhr.responseType = 'document';
+    xhr.send();
 };
+
+getHTML(url, function (response) {
+    console.log(response.querySelector('ol'));
+});
 // let text = document.getElementById('fname').innerText;
 // console.log(text);
 
